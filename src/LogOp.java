3f4dafa9bff123974046fdbc5c897b405eb8dc75
Harini Sivakumar <63298200+Harry12901@users.noreply.github.com>
2020-12-02 00:23:06 +0530

    Update LogOp.java

--- src/LogOp.java
+++ src/LogOp.java
@@ -4,15 +4,51 @@ import java.time.LocalDate;
 import java.util.Scanner;
 public class Logop
 {
-	static ArrayList<Road> Roads = new ArrayList<>();
+    static ArrayList<Road> Roads = new ArrayList<>();
     static ArrayList<Junction> Junctions = new ArrayList<>();
     static ArrayList<Police_Station> PoliceStations = new ArrayList<>();
-    
     static ArrayList<Logs> log  = new ArrayList<>();
     static ArrayList<AccidentLog> alog  = new ArrayList<>();
     static ArrayList<TrafficViolationLog> vlog = new ArrayList<>();
-    static ArrayList<Vehicle> vehicle = new ArrayList<>();
-    
+    static ArrayList<Vehicle> vehicle = new ArrayList<>();	
+	
+     public static Road return_road(int RID){
+        for(int i=0;i<Roads.size();i++){
+            if(RID == Roads.get(i).RoadId){
+                return Roads.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static Junction return_jn(int JID){
+        for(int i=0;i<Junctions.size();i++){
+            if(JID == Junctions.get(i).JunctionID){
+                return Junctions.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static Traffic_Police return_police(Police_Station ps, int PID){
+        ArrayList<Traffic_Police> TrafficPolice = ps.Personnel;
+        for(int i=0;i<TrafficPolice.size();i++){
+            if(PID == TrafficPolice.get(i).PoliceID){
+                return TrafficPolice.get(i);
+            }
+        }
+        return null;
+    }
+
+    public static Police_Station return_police_station(int SID){
+        for(int i=0;i<PoliceStations.size();i++){
+            if(SID == PoliceStations.get(i).StationID){
+                return PoliceStations.get(i);
+            }
+        }
+        return null;
+    }
+      
     public static Logs return_log(int LID){
         for(int i=0;i<log.size();i++){
             if(LID == log.get(i).LogID){
@@ -49,24 +85,7 @@ public class Logop
         return null;
     }
 
-    public static Traffic_Police return_police(Police_Station ps, int PID){
-        ArrayList<Traffic_Police> TrafficPolice = ps.Personnel;
-        for(int i=0;i<TrafficPolice.size();i++){
-            if(PID == TrafficPolice.get(i).PoliceID){
-                return TrafficPolice.get(i);
-            }
-        }
-        return null;
-    }
-
-    public static Police_Station return_police_station(int SID){
-        for(int i=0;i<PoliceStations.size();i++){
-            if(SID == PoliceStations.get(i).StationID){
-                return PoliceStations.get(i);
-            }
-        }
-        return null;
-    }
+   
 
     public static void addViolation(){
         System.out.println("--------------------------");
@@ -82,19 +101,52 @@ public class Logop
         String reason = input.next();
         System.out.println("Enter Penalty amount : ");
         double penalty = input.nextDouble();
-        
-        System.out.println("Enter Road ID : ");
+	System.out.println("Enter Road ID : ");
         int RID = input.nextInt();
-        System.out.println("Enter Police ID : ");
-        int PID = input.nextInt();
-        System.out.println("Enter Station ID : ");
-        int SID = input.nextInt();
+        Road r = return_road(RID);
+        if(r == null){
+            System.err.println("Road does not exist"); 
+           }
+        else
+        {
+        	System.out.println("Enter Station ID : ");
+            int SID = input.nextInt();
+            Police_Station ps = return_police_station(SID);
+            if(ps == null){
+                System.err.println("Police Station does not exist");
+            }
+            else
+            {
+            	System.out.println("Enter Police ID : ");
+                int PID = input.nextInt();            
+                Traffic_Police police = return_police(ps,PID);
+                if(police == null){
+                    System.out.println("Traffic Police does not exist");
+                }
+                else
+                {
+                	vlog.add(new TrafficViolationLog(VID,penalty,reason,regno,speed,RID,PID,SID,"Violation"));
+                	System.out.println("Violation entry added successfully");
+                    System.out.println();
+                    System.out.println("--------------------------");
+                }
+            }
+        }
+        
+      input.close();      
+        
+//         System.out.println("Enter Road ID : ");
+//         int RID = input.nextInt();
+//         System.out.println("Enter Police ID : ");
+//         int PID = input.nextInt();
+//         System.out.println("Enter Station ID : ");
+//         int SID = input.nextInt();
 
-        input.close();
-        vlog.add(new TrafficViolationLog(VID,penalty,reason,regno,speed,RID,PID,SID,"Violation"));
-        System.out.println("Violation entry added successfully");
-        System.out.println();
-        System.out.println("--------------------------");
+//         input.close();
+//         vlog.add(new TrafficViolationLog(VID,penalty,reason,regno,speed,RID,PID,SID,"Violation"));
+//         System.out.println("Violation entry added successfully");
+//         System.out.println();
+//         System.out.println("--------------------------");
     }
     
 
@@ -128,18 +180,49 @@ public class Logop
             }
 
         }
-        
         System.out.println("Enter Road ID : ");
         int RID = input.nextInt();
-        System.out.println("Enter Police ID : ");
-        int PID = input.nextInt();
-        System.out.println("Enter Station ID : ");
-        int SID = input.nextInt();
-        input.close();
-        alog.add(new AccidentLog(AID,damage,reason,ni,nov,vehicles,RID,PID,SID,"Accident"));
-        System.out.println("Accident entry added successfully");
-        System.out.println();
-        System.out.println("--------------------------");
+        Road r = return_road(RID);
+        if(r == null){
+            System.err.println("Road does not exist"); 
+           }
+        else
+        {
+        	System.out.println("Enter Station ID : ");
+            int SID = input.nextInt();
+            Police_Station ps = return_police_station(SID);
+            if(ps == null){
+                System.err.println("Police Station does not exist");
+            }
+            else
+            {
+            	System.out.println("Enter Police ID : ");
+                int PID = input.nextInt();            
+                Traffic_Police police = return_police(ps,PID);
+                if(police == null){
+                    System.out.println("Traffic Police does not exist");
+                }
+                else
+                {
+                	alog.add(new AccidentLog(AID,damage,reason,ni,nov,vehicles,RID,PID,SID,"Accident"));
+                    System.out.println("Accident entry added successfully");
+                    System.out.println();
+                    System.out.println("--------------------------");
+                }
+            }
+        }
+	input.close();    
+//         System.out.println("Enter Road ID : ");
+//         int RID = input.nextInt();
+//         System.out.println("Enter Police ID : ");
+//         int PID = input.nextInt();
+//         System.out.println("Enter Station ID : ");
+//         int SID = input.nextInt();
+//         input.close();
+//         alog.add(new AccidentLog(AID,damage,reason,ni,nov,vehicles,RID,PID,SID,"Accident"));
+//         System.out.println("Accident entry added successfully");
+//         System.out.println();
+//         System.out.println("--------------------------");
     }
     
     
@@ -241,8 +324,6 @@ public class Logop
         System.out.println("--------------------------");
     }
 
-    
-    
     public static void deleteAccident(){
         System.out.println("--------------------------");
         System.out.println();
@@ -273,6 +354,66 @@ public class Logop
         System.out.println();
         System.out.println("--------------------------");
     }
-
+	
+	
+public static void CheckViolation(){
+        System.out.println("--------------------------");
+        System.out.println();
+        System.out.println("Enter Reg number : \n");
+        Scanner input = new Scanner(System.in);
+        String reg = input.next();
+        Vehicle r = return_vehicle(reg);
+        if(r == null){
+            System.err.println("Vehicle does not exist"); 
+           }
+        else
+        {
+        	int flag=0;
+        	for(int i = 0;i<vlog.size();i++){
+        		if(vlog.get(i).regno == reg)
+        		{
+        			System.out.print("Violation ID : ");
+                    System.out.println(vlog.get(i).VID);
+                    System.out.print("Registration Number : ");
+                    System.out.println(vlog.get(i).regno);
+                    System.out.print("Reason : ");
+                    System.out.println(vlog.get(i).Reason);
+                    System.out.print("Speed : ");
+                    System.out.println(vlog.get(i).speed);
+                    System.out.print("Penalty amount : ");
+                    System.out.println(vlog.get(i).PenaltyAmount);
+                    System.out.print("Road ID : ");
+                    System.out.println(vlog.get(i).RoadID);
+                    System.out.print("Police ID : ");
+                    System.out.println(vlog.get(i).PoliceID);
+                    System.out.print("Station ID : ");
+                    System.out.println(vlog.get(i).StationID);
+                    System.out.print("Date : ");
+                    System.out.println(vlog.get(i).date);
+                    System.out.print("Time : ");
+                    System.out.println(vlog.get(i).time);
+                    System.out.println("--------------------------");
+                    System.out.println("Pay Penalty (y/n) :");
+                    String inp = input.next();
+                    if(inp=="y")
+                    {
+                    	deleteViolation();
+                    }
+                    
+                    System.out.println();
+                    flag=1;
+        		} 
+            }
+        	if(flag==0)
+        	{
+        		System.out.println("No Violation record found");
+        	}
+        	
+        }
+        
+        System.out.println("--------------------------");
+        input.close();
+    }
+    
 
 }
