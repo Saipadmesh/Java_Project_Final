44b07b7b0ebcfcda7851ae539760fae053c9f6bb
Gomathi <59409466+GomathiRaveendran@users.noreply.github.com>
2020-12-06 14:46:36 +0530

    Add files via upload

---
+++ src/Login.java
@@ -0,0 +1,192 @@
+package semproject;
+import java.util.ArrayList;
+import java.util.Scanner;
+public class Login {
+	Scanner s = new Scanner(System.in);
+	ArrayList<User> userlist = new ArrayList<User>();
+	void modify(String chu)
+	{
+		int flag = -1;
+		int x = search(chu);
+		System.out.println("Modify username (1) or (2) password (3) authorization ?");
+		int c = Integer.parseInt(s.nextLine());
+		String y = null;
+		switch(c)
+		{
+			case 1: System.out.println("Enter new username");
+					y = s.nextLine();
+					if(search(y)==-1)
+					{
+						if(y.length()>=6) {
+							flag = 1;
+						}
+					}
+					break;
+			case 2: System.out.println("Enter new password");
+					if(y.length()>=6)
+					{
+						String numRegex   = ".*[0-9].*";
+						String alphaRegex = ".*[A-Z].*";
+							if (y.matches(numRegex) && y.matches(alphaRegex)) 
+							{
+								flag = 1;
+							}
+					}
+					break;
+			case 3: try {
+				        int d = Integer.parseInt(y);
+				    } catch (NumberFormatException nfe) {
+				        flag = -1;
+				    }
+				    flag = 1;
+				    break;
+		}
+		if(flag == 1) {
+			userlist.get(x).modify(c, y);
+			}
+	}
+	void delete(String chu)
+	{
+		// PLEASE MAKE SURE USERNAME EXISTS BEFORE CALLING THIS FUNCTION !!
+		int x = search(chu);
+		userlist.remove(x);
+	}
+	
+	int search(String chu)
+	{
+		for(int i = 0; i < userlist.size(); i++)
+		{
+			if(userlist.get(i).getusn() == chu)
+					return i;
+		}
+		return -1;
+	}
+	boolean signin(String chu, String chp)
+	{
+		int x= search(chu);
+		if(x!=-1){
+			if(userlist.get(x).getpwd() == userlist.get(x).hash(chp))
+			{
+				return true;
+			}
+			else return false;
+		}
+		else return false;
+	}
+	public static boolean isNumeric(String str) { 
+		  try {  
+		    Double.parseDouble(str);  
+		    return true;
+		  } catch(NumberFormatException e){  
+		    return false;  
+		  }  
+		}
+	boolean signup(String chu, String chp)
+	{
+		if(chu!= null && chp!= null)
+		{
+			if(chu.length()>=6 && chp.length()>=6)
+			{
+				if(search(chu)==-1)
+				{
+					String numRegex   = ".*[0-9].*";
+					String alphaRegex = ".*[A-Z].*";
+
+					if (chp.matches(numRegex) && chp.matches(alphaRegex)) 
+					{
+						userlist.add(new User(chu,chp,0));
+						System.out.println("+------------------------------------------------+");
+						System.out.println("|       USER ACCOUNT CREATED SUCCESSFULLY!       |");
+						System.out.println("+------------------------------------------------+");
+						return true;
+					}
+					else {
+
+						System.err.println("+------------------------------------------------+");
+						System.err.println("|  PASSWORD NEEDS TO BE LONGER THAN SIX DIGITS   |");
+						System.err.println("|AND MUST CONTAIN NUMBERS AND UPPERCASE ALPHABETS|");
+						System.err.println("+------------------------------------------------+");
+						return false;
+					}
+
+				}
+				else {
+
+					System.err.println("+------------------------------------------------+");
+					System.err.println("|      USERNAME ALREADY IN USE, TRY AGAIN.       |");
+					System.err.println("+------------------------------------------------+");
+					return false;
+					}
+			}
+			else {
+
+				System.err.println("+------------------------------------------------+");
+				System.err.println("|           PASSWORD AND USERNAME                |");
+				System.err.println("|     NEEDS TO BE LONGER THAN SIX DIGITS         |");
+				System.err.println("+------------------------------------------------+");
+				return false;
+				}
+		}
+		else {
+
+			System.err.println("+------------------------------------------------+");
+			System.err.println("|              PASSWORD AND USERNAME             |");
+			System.err.println("|                 CANNOT BE NULL                 |");
+			System.err.println("+------------------------------------------------+");
+			return false;
+			}
+	}
+	public Login()
+	{
+		
+		System.out.println("    +---------------------------------------+");
+		System.out.println("    |  WELCOME! PLEASE LOGIN IN OR SIGN UP  |");
+		System.out.println("    +---------------------------------------+");
+		
+		System.out.println("+----------------+                +-----------------+ ");
+		System.out.println("|Enter 1 to login|                |Enter 2 to signup|");
+		System.out.println("+----------------+                +-----------------+ ");
+		System.out.println("===================================================== ");
+		int c = Integer.parseInt(s.nextLine());
+		if(c == 1 || c==2)
+		{
+			System.out.println("+------------------------------------------------+");
+			System.out.println("|             ENTER USERNAME                     |");
+			System.out.println("+------------------------------------------------+");
+			String x = s.nextLine();
+			System.out.println("+------------------------------------------------+");
+			System.out.println("|           ENTER PASSWORD                       |");
+			System.out.println("+------------------------------------------------+");
+			String y = s.nextLine();
+			if(c==1)
+			{
+				boolean z = signin(x,y);
+				if(z)
+				{
+					System.out.println("+------------------------------------------------+");
+					System.out.println("|                LOGIN SUCCESSFULL!              |");
+					System.out.println("+------------------------------------------------+");
+				}
+				else {
+					System.out.println("+------------------------------------------------+");
+					System.err.println("|          INVALID USERNAME OR PASSWORD          |");
+					System.out.println("+------------------------------------------------+");
+				}
+			}
+			else if(c == 2)
+		{
+			signup(x,y);
+		}
+		else {
+			System.out.println("+------------------------------------------------+");
+			System.err.println("|          INVALID INPUT. TRY AGAIN.             |");
+			System.out.println("+------------------------------------------------+");
+		}
+		}
+		
+	}
+	public static void main(String args[])
+	{
+		Login g = new Login();
+	}
+}
---
+++ src/Person.java
@@ -0,0 +1,18 @@
+package semproject;
+
+public class Person {
+	String name;
+	int age;
+	char sex;
+	long aadhar;
+	String address;
+	public Person(String n, int a, char s, long aad, String addr )
+	{
+		name = n;
+		age = a;
+		sex = s;
+		aadhar = aad;
+		address = addr;
+	}
+
+}
